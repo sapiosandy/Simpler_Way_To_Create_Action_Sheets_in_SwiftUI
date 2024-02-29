b0c38006b39f6fe7342dd0d91ef67fc553da35a0
Sandy Gomez <sgomez1029@gmail.com>
2024-02-29 09:19:18 -0800

    Simpler Way to Create Action Sheets in SwiftUI (UCSD Extension)

--- Simpler_Way_To_Create_Action_Sheets_in_SwiftUI/ContentView.swift
+++ Simpler_Way_To_Create_Action_Sheets_in_SwiftUI/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showActionSheet = false
+    @State private var buttonTapped = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 25) {
+            Text("\(buttonTapped)")
+            Button(action: {
+                showActionSheet.toggle()
+            }) {
+                Text("Show Alert")
+            }
+            .confirmationDialog("", isPresented: $showActionSheet) {
+                Button("Dog") {
+                    buttonTapped = "Dog"
+                }
+                Button("Cat") {
+                    buttonTapped = "Cat"
+                }
+                Button("Bird") {
+                    buttonTapped = "Bird"
+                }
+            }
         }
-        .padding()
     }
 }
 
