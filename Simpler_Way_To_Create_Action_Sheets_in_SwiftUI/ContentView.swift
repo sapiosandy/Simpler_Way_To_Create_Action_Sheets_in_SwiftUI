c491869433ee14c1299e5e5181b5f9020d87fd3b
Sandy Gomez <sgomez1029@gmail.com>
2024-02-29 09:11:31 -0800

    Initial Commit

---
+++ Simpler_Way_To_Create_Action_Sheets_in_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simpler_Way_To_Create_Action_Sheets_in_SwiftUI
+//
+//  Created by Sandra Gomez on 2/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simpler_Way_To_Create_Action_Sheets_in_SwiftUI/Simpler_Way_To_Create_Action_Sheets_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simpler_Way_To_Create_Action_Sheets_in_SwiftUIApp.swift
+//  Simpler_Way_To_Create_Action_Sheets_in_SwiftUI
+//
+//  Created by Sandra Gomez on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Simpler_Way_To_Create_Action_Sheets_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
